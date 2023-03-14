57223d942483b3bfc06d1a4d67a99f1ad6086ce4
Canacepa <cassandre.lavallard@gmail.com>
2023-03-14 17:09:11 +0100

    need css

--- app.js
+++ app.js
@@ -2,27 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static('public'));
-
-
+app.use(express.static("public"));
 
 app.get("/", (request, response, next) => {
-    response.sendFile(__dirname + '/views/home.html');
+  response.sendFile(__dirname + "/views/home.html");
 });
 
 app.get("/about", (request, response, next) => {
-    response.sendFile(__dirname + '/views/about.html');
-})
+  response.sendFile(__dirname + "/views/about.html");
+});
 
 app.get("/work", (request, response, next) => {
-    response.sendFile(__dirname + '/views/work.html');
-})
+  response.sendFile(__dirname + "/views/work.html");
+});
 
-app.get("/about", (request, response, next) => {
-    response.sendFile(__dirname + '/views/gallery.html');
-})
+app.get("/gallery", (request, response, next) => {
+  response.sendFile(__dirname + "/views/gallery.html");
+});
 
-
-
-
-app.listen(3001, () => { console.log("server listening on port 3001")});
+app.listen(3001, () => {
+  console.log("server listening on port 3001");
+});
